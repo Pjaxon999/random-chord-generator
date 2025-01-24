36e2e0375bebd6a66572d9e244fc7ae9f51f2625
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-01-24 15:05:58 -0700

    Finished Basic Script for Generating Random Chords
    The chord generator script has been added. It utilizes two arrays, one
    with the Keys (I did not include enharmonic keys for this part), and
    another with the chord qualities. When the button is clicked, the
    function to pick a random chord is called. The function takes a random
    index from both of the arrays and combines them, then updates the text
    content of the pargraph above the button. Because the function uses the
    length of the arrays to "pick" a random chord, I can add more chord
    qualities or keys as necessary. I may add functionality in the future to
    change these settings, but for now I am going to stick with just the
    most commonly used keys, and only the chord qualities that I can
    currently play on command.

--- scripts.js
+++ scripts.js
@@ -1,2 +1,20 @@
 // Test external link
 console.log("This is the chord randomizer speaking.")
+
+// Grab the button and chord output area from the DOM
+const chordButton = document.getElementById("chord-button");
+let chordName = document.getElementById("chord-name")
+
+// Arrays containing Keys and Chord Qualities
+const keys = ["A", "A#","Ab","B","Bb","C","C#","D","Db","D#","E","Eb","F","F#","G","Gb","G#"]
+const qualities = ["", "min", "maj7", "min7", "7"]
+
+// Randomly Select a key and a quality, then update the chord-name element
+let keysIndex
+let qualitiesIndex
+
+chordButton.addEventListener("click", function() {
+    keysIndex = keys[Math.floor(Math.random() * keys.length)];
+    qualitiesIndex = qualities[Math.floor(Math.random() * qualities.length)];
+    chordName.textContent = `${keysIndex}${qualitiesIndex}`;
+});
